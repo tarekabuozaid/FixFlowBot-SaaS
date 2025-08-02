017ef72d8dcc8a687b2abcc71d7cb7f9405057e9
tarekabuozaid <tarekabuzaidcafm@outlook.com>
2025-08-02 11:51:57 +0400

    chore: rename ESLint config to .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+  root: true,
+  env: { es6: true, browser: true, node: false },
+  globals: {
+    SpreadsheetApp: 'readonly',
+    PropertiesService: 'readonly',
+    DriveApp: 'readonly',
+    UrlFetchApp: 'readonly',
+    ScriptApp: 'readonly',
+    HtmlService: 'readonly',
+    Logger: 'readonly'
+  },
+  extends: 'eslint:recommended',
+  rules: { 'no-unused-vars': 'off', 'no-undef': 'off' }
+};
